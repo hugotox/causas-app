3a18db035b1c8bdc359c578f0ff9c2bf8739e7c3
hugotox <hpineda83@gmail.com>
2017-04-05 10:45:59 -0300

    react native upgrade

--- android/app/src/main/java/com/causasapp/MainApplication.java
+++ android/app/src/main/java/com/causasapp/MainApplication.java
@@ -1,7 +1,6 @@
 package com.causasapp;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
